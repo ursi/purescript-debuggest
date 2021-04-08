c57b867dd6403dba3223e5db28c82c351757f4df
Mason Mackaman <masondeanm@aol.com>
2021-04-08 18:48:13 -0400

    Add rev

--- package.nix
+++ package.nix
@@ -2,6 +2,6 @@
   with ps-pkgs;
   { version = "0.4.0";
     repo = "https://github.com/ursi/purescript-debuggest.git";
-    rev = "";
+    rev = "0bbfa416ebdf7383b89f2471dda9ef22bc2c3028";
     dependencies = [ prelude unsafe-coerce ];
   }
